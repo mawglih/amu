335ca1dd8f081a7191a0dd8dc870b894228dc9be
Oleg Markoff <omarkoff@1800flowers.com>
2022-10-23 08:52:17 -0400

    update 2022

--- src/constants/aws.js
+++ src/constants/aws.js
@@ -1,6 +1,6 @@
 export default {
   root: 'https://amu-images.s3.us-east-2.amazonaws.com/',
   fotos: [
-    'kayut1','kayuta2','kreslo','kreslo2','enc-blue1','enc-blue2','lodka1','lodka2','lodka3','seat-wb-1','seat-wb-3','bar1a', 'bar1b', 'bar2a', 'bar2b', 'captain_chair_a', 'captain_chair_b', 'siduxa_a', 'siduxa_b', 'cushion_a', 'cushion_b', 'flooring_a', 'flooring_b', 'living_a', 'living_b', 'panels_a', 'panels_b' ,'lodka','EN1', 'EN2', 'EN3', 'EN4', 'EN5', 'EN6', 'UP1', 'UP2', 'UP3', 'UP4', 'UP5', 'UP6', 'UP7', 'skama', 'skamb', 'diva', 'divb'
+    'upd2022-1','upd2022-2','upd2022-3','upd2022-4','upd2022-5','upd2022-6','upd2022-7','upd2022-8','upd2022-9','upd2022-10','upd2022-11','upd2022-12','upd2022-13','upd2022-14','kayut1','kayuta2','kreslo','kreslo2','enc-blue1','enc-blue2','lodka1','lodka2','lodka3','seat-wb-1','seat-wb-3','bar1a', 'bar1b', 'bar2a', 'bar2b', 'captain_chair_a', 'captain_chair_b', 'siduxa_a', 'siduxa_b', 'cushion_a', 'cushion_b', 'flooring_a', 'flooring_b', 'living_a', 'living_b', 'panels_a', 'panels_b' ,'lodka','EN1', 'EN2', 'EN3', 'EN4', 'EN5', 'EN6', 'UP1', 'UP2', 'UP3', 'UP4', 'UP5', 'UP6', 'UP7', 'skama', 'skamb', 'diva', 'divb'
   ]
 }
